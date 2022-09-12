2daecb99105e6106df063ba8a8072e4f53a46f49
Dayroot <dallancaceresp@gmail.com>
2022-09-12 10:57:58 -0500

    Add a scope

--- src/db/models/shoppingCart.model.js
+++ src/db/models/shoppingCart.model.js
@@ -1,6 +1,42 @@
 const conn = require('../connectionDB');
 
+//Models
+const Product = require('./product.model');
+const ShoppingCart_Product = require('./shoppingCart_product.model');
+const Brand = require('./brand.model');
+const ProductImage = require('./productImage.model');
+
 const ShoppingCart = conn.define('shoppingCart', {}, {
+	timestamps: false,
+	scopes: {
+		products: {
+			include: {
+				model: ShoppingCart_Product,
+				attributes: {
+					exclude: ['shoppingCartId', 'productId'],
+				},
+				include: {
+					model: Product,
+					include: [
+						{
+							model: Brand,
+						},
+						{
+							model: ProductImage,
+							as: 'images',
+							attributes: {
+								exclude: ['productId']
+							},
+						}
+					],
+					attributes: {
+						exclude: ['brandId']
+					},
+				}
+			},
+		}
+	}
+}, {
 	timestamps: false,
 });
 
