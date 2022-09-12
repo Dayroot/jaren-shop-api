6674fe8bc230ed2891b1e4d69d3f56010b556027
Dayroot <dallancaceresp@gmail.com>
2022-09-12 10:55:54 -0500

    Renaming timestamps and implementing a scope

--- src/db/models/purchase.model.js
+++ src/db/models/purchase.model.js
@@ -1,20 +1,49 @@
 const {DataTypes} = require('sequelize');
 const conn = require('../connectionDB');
 
+//Models
+const Product = require('./product.model');
+const Brand = require('./brand.model');
+const ProductImage = require('./productImage.model');
+const Purchase_Product = require('./purchase_product.model');
+
 const Purchase = conn.define( 'purchase', {
-	date: {
-		type: DataTypes.DATE,
-		defaultValue: DataTypes.NOW,
-		validate: {
-			isDate: true,
-		}
-	},
 	status: {
 		type: DataTypes.ENUM('pending', 'dispatched', 'delivered'),
 		defaultValue: 'pending',
 	}
 }, {
-	timestamps: false,
+	timestamps: true,
+	updatedAt: 'statusChangeDate',
+	createdAt: 'purchaseDate',
+	scopes: {
+		products: {
+			include: {
+				model: Purchase_Product,
+				attributes: {
+					exclude: ['purchaseId', 'productId']
+				},
+				include: {
+					model: Product,
+					include: [
+						{
+							model: Brand,
+						},
+						{
+							model: ProductImage,
+							as: 'images',
+							attributes: {
+								exclude: ['productId']
+							},
+						}
+					],
+					attributes: {
+						exclude: ['brandId']
+					},
+				}
+			}
+		}
+	}
 });
 
 module.exports = Purchase;
