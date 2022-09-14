a9aa89adfe40de64f25899d80e2e1da80f23123e
Dayroot <dallancaceresp@gmail.com>
2022-09-14 16:15:35 -0500

    edit scope products

--- src/db/models/purchase.model.js
+++ src/db/models/purchase.model.js
@@ -1,4 +1,4 @@
-const {DataTypes} = require('sequelize');
+const {DataTypes, Op} = require('sequelize');
 const conn = require('../connectionDB');
 
 //Models
@@ -8,6 +8,7 @@ const ProductImage = require('./productImage.model');
 const Purchase_Product = require('./purchase_product.model');
 const Category = require('./category.model');
 const Price = require('./price.model');
+const OrderAddress = require('./OrderAddress.model');
 
 const Purchase = conn.define( 'purchase', {
 	status: {
@@ -20,39 +21,48 @@ const Purchase = conn.define( 'purchase', {
 	createdAt: 'purchaseDate',
 	scopes: {
 		products: {
-			include: {
-				model: Purchase_Product,
-				attributes: {
-					exclude: ['purchaseId', 'productId']
-				},
-				include: {
-					model: Product,
-					include: [
-						{
-							model: Brand,
-						},
-						{
-							model: ProductImage,
-							as: 'images',
-							attributes: {
-								exclude: ['productId']
-							},
-						},
-						{
-							model: Price,
-							attributes: {
-								exclude: ['productId']
-							}
-						},
-						{
-							model: Category,
-						}
-					],
+			include: [
+				{
+					model: OrderAddress,
+					as: 'address',
 					attributes: {
-						exclude: ['brandId', 'categoryId']
+						exclude: ['purchaseId', 'id'],
+					}
+				},
+				{
+					model: Purchase_Product,
+					attributes: {
+						exclude: ['purchaseId', 'productId']
 					},
+					include: {
+						model: Product,
+						include: [
+							{
+								model: Brand,
+							},
+							{
+								model: ProductImage,
+								as: 'images',
+								attributes: {
+									exclude: ['productId']
+								},
+							},
+							{
+								model: Price,
+								attributes: {
+									exclude: ['productId']
+								}
+							},
+							{
+								model: Category,
+							}
+						],
+						attributes: {
+							exclude: ['brandId', 'categoryId']
+						},
+					}
 				}
-			}
+			]
 		}
 	}
 });
