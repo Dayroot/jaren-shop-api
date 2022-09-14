82526fad6528744997786dc0ab71aa44fd7cdd6d
Dayroot <dallancaceresp@gmail.com>
2022-09-14 16:22:43 -0500

    Add the address paramater in the test

--- test/models/purchase.test.js
+++ test/models/purchase.test.js
@@ -25,32 +25,20 @@ describe('Purchase model', () => {
 	describe('Validations', () => {
 
 		it('If status property is null, the record will be created and the status value must be pending', () =>
-			PurchaseModel.create({addressId: 1})
+			PurchaseModel.create({})
 				.then( (purchase) => {
 					expect(purchase.status).toEqual('pending');
 				})
 		);
 
 		it('If status property value is diferent to (pending, dispatched or delivered), the record will not be created', (done) => {
-			PurchaseModel.create({status: 'Any', addressId: 1})
-			.then( () => done("The record should not have been created"))
-			.catch( () => done());
-		});
-
-		it('If addressId property is null, the record will not be created', (done) => {
-			PurchaseModel.create({status: 'pending'})
-			.then( () => done("The record should not have been created"))
-			.catch( () => done());
-		});
-
-		it('If addressId property is not integer type, the record will not be created', (done) => {
-			PurchaseModel.create({status: 'pending', addressId: 'test'})
+			PurchaseModel.create({status: 'Any'})
 			.then( () => done("The record should not have been created"))
 			.catch( () => done());
 		});
 
 		it('If the parameters are correct, the record should be created in the database', (done) => {
-			PurchaseModel.create({status: 'pending', addressId: 1})
+			PurchaseModel.create({status: 'pending'})
 			.then( () => done())
 			.catch( () => done("The record should have been created"));
 		});
