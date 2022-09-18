47bb3a2372ecdf27d4df5695d71480ec326f47b2
Dayroot <dallancaceresp@gmail.com>
2022-09-17 22:57:40 -0500

    Implement test for product routes

--- test/routes/products.test.js
+++ test/routes/products.test.js
@@ -35,7 +35,7 @@ describe('Products Endpoints', () => {
 	});
 
 	describe('POST /', () => {
-		it('Respond with a status code of 200', () =>
+		it('Respond with a status code of 201', () =>
 			agent
 				.post("/api/v1/products")
 				.send(productsData[0])
@@ -58,11 +58,11 @@ describe('Products Endpoints', () => {
 					expect(Object.keys(product.variants[0]).sort()).toEqual(['id', 'price', 'stock', 'size', 'SKU', 'UPC'].sort());
 				})
 		);
-		it('Responds with Error', () =>
+		it('If parameters are missing, responds with Error 400', () =>
 			agent
 				.post("/api/v1/products")
 				.send({name: "test"})
-				.expect(500)
+				.expect(400)
 				.then( res => {
 					const {error, body} = res.body;
 					expect(typeof error).toBe('string');
@@ -87,9 +87,8 @@ describe('Products Endpoints', () => {
 
 					expect(Object.keys(res.body)).toEqual(['error', 'body']);
 					expect(Array.isArray(res.body.body)).toBeTruthy();
-					const products =  res.body.body;
 
-					products.forEach( (product, i) => {
+					res.body.body.forEach( (product, i) => {
 						expect(typeof product).toBe('object');
 						expect(product.id).toBe(i+1);
 						expect(product.images.length).toBe(3);
@@ -128,10 +127,21 @@ describe('Products Endpoints', () => {
 				})
 		);
 
-		it('Responds with Error', () =>
+		it('If the id does not correspond to any product, responds with Error 404', () =>
 			agent
 				.get("/api/v1/products/62")
 				.expect(404)
+				.then( res => {
+					const {error, body} = res.body;
+					expect(typeof error).toBe('string');
+					expect(body === null).toBeTruthy();
+				})
+		);
+
+		it('If the id is not an integer, responds with Error 400', () =>
+			agent
+				.get("/api/v1/products/test")
+				.expect(400)
 				.then( res => {
 					const {error, body} = res.body;
 					expect(typeof error).toBe('string');
@@ -152,14 +162,14 @@ describe('Products Endpoints', () => {
 					name: "New name",
 					variants: [
 						{
-							id: 1,
+							id: 2,
 							size: '800',
 						}
 					]
 				})
 				.expect(200)
 		);
-		it('Responds with the product data that corresponds to the id', () =>
+		it('Update the product that correspond the id indicated and responds with the product data', () =>
 			agent
 				.patch("/api/v1/products/1")
 				.send({
@@ -187,7 +197,7 @@ describe('Products Endpoints', () => {
 				})
 		);
 
-		it('Responds with the product data that corresponds to the id', () =>
+		it('Update the product that correspond the id indicated and responds with the product data', () =>
 			agent
 				.patch("/api/v1/products/1")
 				.send({
@@ -208,7 +218,7 @@ describe('Products Endpoints', () => {
 				})
 		);
 
-		it('Responds with Error', () =>
+		it('If the id does not correspond to any product, responds with Error 404', () =>
 			agent
 				.patch("/api/v1/products/62")
 				.send({
@@ -216,6 +226,58 @@ describe('Products Endpoints', () => {
 					name: "New name",
 				})
 				.expect(404)
+				.then( res => {
+					const {error, body} = res.body;
+					expect(typeof error).toBe('string');
+					expect(body === null).toBeTruthy();
+				})
+		);
+
+		it('If the variant id does not correspond to any variant, responds with Error 404', () =>
+			agent
+				.patch("/api/v1/products/1")
+				.send({
+					description: "The description was updated",
+					name: "New name",
+					variants: [
+						{
+							id: 81,
+							size: '800',
+						}
+					]
+				})
+				.expect(404)
+				.then( res => {
+					const {error, body} = res.body;
+					expect(typeof error).toBe('string');
+					expect(body === null).toBeTruthy();
+				})
+		);
+
+		it('If the id is not an integer, responds with Error 400', () =>
+			agent
+				.patch("/api/v1/products/test")
+				.send({
+					description: "The description was updated",
+					name: "New name",
+				})
+				.expect(400)
+				.then( res => {
+					const {error, body} = res.body;
+					expect(typeof error).toBe('string');
+					expect(body === null).toBeTruthy();
+				})
+		);
+
+		it('If the data contains the images property, responds with Error 400', () =>
+			agent
+				.patch("/api/v1/products/1")
+				.send({
+					description: "The description was updated",
+					name: "New name",
+					images: [],
+				})
+				.expect(400)
 				.then( res => {
 					const {error, body} = res.body;
 					expect(typeof error).toBe('string');
@@ -242,9 +304,20 @@ describe('Products Endpoints', () => {
 				})
 		);
 
-		it('Responds with Error', () =>
+		it('If the id does not correspond to any product, responds with Error 404', () =>
 			agent
 				.delete("/api/v1/products/62")
+				.expect(404)
+				.then( res => {
+					const {error, body} = res.body;
+					expect(typeof error).toBe('string');
+					expect(body === null).toBeTruthy();
+				})
+		);
+
+		it('If the id is not an integer, responds with Error 400', () =>
+			agent
+				.delete("/api/v1/products/test")
 				.expect(400)
 				.then( res => {
 					const {error, body} = res.body;
