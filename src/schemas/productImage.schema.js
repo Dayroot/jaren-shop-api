5c5979ad50ed9b862ff2284e7052ab5544b032b9
Dayroot <dallancaceresp@gmail.com>
2022-09-17 22:59:43 -0500

    Implement schema for product image data

---
+++ src/schemas/productImage.schema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const url = Joi.string();
+
+const createImageSchema = Joi.object({
+	url: url.required(),
+});
+
+const updateImageSchema = Joi.object({
+	id: id.required(),
+	url: url,
+});
+
+const getImageSchema = Joi.object({
+	id: id.required(),
+});
+
+
+module.exports = {
+	createImageSchema,
+	updateImageSchema,
+	getImageSchema,
+}
