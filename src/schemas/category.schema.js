7b8a149f41ac6ca829e4b6e6c813d382c93ad8db
Dayroot <dallancaceresp@gmail.com>
2022-09-22 21:58:58 -0500

    Implement schema for category data

---
+++ src/schemas/category.schema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string().min(1);
+
+const createCategorySchema = Joi.object({
+	name: name.required(),
+});
+
+const updateCategorySchema = Joi.object({
+	id: id.required(),
+	name: name,
+});
+
+const getCategorySchema = Joi.object({
+	id: id.required(),
+});
+
+
+module.exports = {
+	createCategorySchema,
+	updateCategorySchema,
+	getCategorySchema,
+};
