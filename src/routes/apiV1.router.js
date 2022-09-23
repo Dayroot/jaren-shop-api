3c3a2564c5addb59cffca3956c01731d87c3f0f6
Dayroot <dallancaceresp@gmail.com>
2022-09-22 22:00:56 -0500

    Add category router

--- src/routes/apiV1.router.js
+++ src/routes/apiV1.router.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 //Routers
 const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
 
 router.use('/products', productsRouter);
+router.use('/categories', categoriesRouter);
 
 module.exports = router;
