f28579f0dd22e75f57e2048056a53b8d107feb55
Dayroot <dallancaceresp@gmail.com>
2022-09-22 22:39:03 -0500

    Add brand routes

--- src/routes/apiV1.router.js
+++ src/routes/apiV1.router.js
@@ -4,8 +4,10 @@ const router = express.Router();
 //Routers
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
+const brandsRouter = require('./brands.router');
 
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
+router.use('/brands', brandsRouter);
 
 module.exports = router;
