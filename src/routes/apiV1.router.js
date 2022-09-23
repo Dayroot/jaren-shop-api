191916a47e550bb97a3e0959a3f7b3f505621160
Dayroot <dallancaceresp@gmail.com>
2022-09-22 23:54:30 -0500

    Add the discounts router

--- src/routes/apiV1.router.js
+++ src/routes/apiV1.router.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const brandsRouter = require('./brands.router');
+const discountsRouter = require('./discounts.router');
 
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
 router.use('/brands', brandsRouter);
+router.use('/discounts', discountsRouter);
 
 module.exports = router;
