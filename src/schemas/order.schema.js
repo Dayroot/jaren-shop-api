17aee536a06ea01811d2362cd012ba9181bed700
Dayroot <dallancaceresp@gmail.com>
2022-09-24 12:26:32 -0500

    Fix bug in update schema

--- src/schemas/order.schema.js
+++ src/schemas/order.schema.js
@@ -1,25 +1,24 @@
 const Joi = require('joi');
-const { createOrderAddressSchema } = require('./address.schema');
-const { createDetailSchema } = require('./orderDetail.schema');
+const { createOrderAddressSchema, updateAddressSchema } = require('./address.schema');
+const { createDetailSchema, updateDetailSchema } = require('./orderDetail.schema');
 
 const id = Joi.number().integer();
 const userId = Joi.number().integer();
 const status = Joi.string().valid('pending', 'dispatched', 'delivered');
-const details = Joi.array().items( createDetailSchema );
 
 const createOrderSchema = Joi.object({
 	userId: userId.required(),
 	address: createOrderAddressSchema.required(),
 	status: status.required(),
-	details: details.required(),
+	details: Joi.array().items( createDetailSchema ).required(),
 });
 
 const updateOrderSchema = Joi.object({
 	id: id.required(),
 	userId,
-	address: createOrderAddressSchema.forbidden(),
+	address: updateAddressSchema.forbidden(),
 	status,
-	details,
+	details: Joi.array().items( updateDetailSchema ),
 });
 
 const getOrderSchema = Joi.object({
