00935fe442aa76a551a424e49c092bda63529454
Dayroot <dallancaceresp@gmail.com>
2022-09-24 11:09:03 -0500

    Add the order router

--- src/routes/apiV1.router.js
+++ src/routes/apiV1.router.js
@@ -8,6 +8,7 @@ const brandsRouter = require('./brands.router');
 const discountsRouter = require('./discounts.router');
 const usersRouter = require('./users.router');
 const addressesRouter = require('./addresses.router');
+const ordersRouter = require('./orders.router');
 
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
@@ -15,5 +16,6 @@ router.use('/brands', brandsRouter);
 router.use('/discounts', discountsRouter);
 router.use('/users', usersRouter);
 router.use('/addresses', addressesRouter);
+router.use('/orders', ordersRouter);
 
 module.exports = router;
