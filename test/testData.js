f81a6992ac112dd1fec3ca143543bf97ad61ad91
Dayroot <dallancaceresp@gmail.com>
2022-09-26 12:21:36 -0500

    Add data for all tests

--- test/testData.js
+++ test/testData.js
@@ -55,3 +55,205 @@ exports.productsData = [
 		categoryId: 1,
 	}
 ];
+
+exports.categoriesData = [
+	{
+		name: "perfumes"
+	},
+	{
+		name: "watches"
+	},
+];
+
+exports.discountsData = [
+	{
+		value: 30,
+		startDate: "2021-07-02T16:17:01.559Z",
+		finishDate: "2022-09-02T16:17:01.559Z",
+		isEnabled: true,
+	},
+	{
+		value: 40,
+		startDate: "2021-07-02T16:17:01.559Z",
+		finishDate: "2022-09-02T16:17:01.559Z",
+		isEnabled: true,
+	},
+	{
+		value: 60,
+		startDate: "2021-07-02T16:17:01.559Z",
+		finishDate: "2022-09-02T16:17:01.559Z",
+		isEnabled: true,
+	}
+];
+
+exports.usersData = [
+	{
+		firstName: 'Juanito',
+		lastName: 'Perez',
+		email: 'juenito@gmail.com',
+		password: '12345',
+	},
+	{
+		firstName: 'Carlos',
+		lastName: 'Carrillo',
+		email: 'carlos@gmail.com',
+		password: '54321',
+	},
+];
+
+exports.addressesData = [
+	{
+		userId: 1,
+		state: 'Norte de santander',
+		city: 'Cucuta',
+		streetAddress: 'avenida 1a #85d barrio Caobos',
+		postalCode: '530001',
+		propertyType: 'house',
+		phoneNumber: '3164578632',
+		fullname: 'Helena Blade',
+	},
+	{
+		userId: 1,
+		state: 'Antioquia',
+		city: 'Medellin',
+		streetAddress: 'avenida 17a #45-3 Centro',
+		postalCode: '530001',
+		propertyType: 'apartment',
+		phoneNumber: '3164578632',
+		fullname: 'Claudio Fonseca',
+	},
+];
+
+
+exports.ordersData = [
+	{
+		userId: 1,
+		address: {
+			state: 'Norte de santander',
+			city: 'Cucuta',
+			streetAddress: 'avenida 1a #85d barrio Caobos',
+			postalCode: '530001',
+			propertyType: 'house',
+			phoneNumber: '3164578632',
+			fullname: 'Helena Blade',
+		},
+		status: 'pending',
+		details: [
+			{
+				productId: 1,
+				SKU: "DIO-SAU-30",
+				price: 350.01,
+				quantity: 1,
+			},
+			{
+				productId: 2,
+				SKU: "CHA-BLE-100",
+				price: 1149.99,
+				quantity: 4,
+			},
+			{
+				productId: 3,
+				SKU:"YSL-BOP-60",
+				price: 1950.01,
+				quantity: 2,
+			}
+		],
+	},
+	{
+		userId: 1,
+		address: {
+			state: 'Norte de santander',
+			city: 'Cucuta',
+			streetAddress: 'avenida 1a #85d barrio Caobos',
+			postalCode: '530001',
+			propertyType: 'house',
+			phoneNumber: '3164578632',
+			fullname: 'Helena Blade',
+		},
+		status: 'pending',
+		details: [
+			{
+				productId: 1,
+				SKU: "DIO-SAU-30",
+				price: 350.01,
+				quantity: 1,
+			},
+			{
+				productId: 2,
+				SKU: "CHA-BLE-100",
+				price: 1149.99,
+				quantity: 4,
+			},
+		],
+	},
+];
+
+exports.productImagesData = [
+	{url: "http://productimage1.png"},
+	{url: "http://productimage2.png"},
+	{url: "http://productimage3.png"},
+];
+
+exports.reviewsData = [
+	{
+		productId: 1,
+		userId: 1,
+		ref: 1,
+		rating: 5,
+		text: "This products is good",
+	},
+	{
+		productId: 1,
+		userId: 1,
+		ref: 2,
+		rating: 3,
+		text: "This products is regular",
+	},
+	{
+		productId: 1,
+		userId: 1,
+		ref: 3,
+		rating: 1,
+		text: "This products is bad",
+	},
+];
+
+exports.shoppingProducts = [
+	{
+		shoppingCartId: 1,
+		productId: 1,
+		SKU:"DIO-SAU-30",
+		quantity: 1,
+	},
+	{
+		shoppingCartId: 1,
+		productId: 2,
+		SKU:"CHA-BLE-60",
+		quantity: 5,
+	},
+	{
+		shoppingCartId: 1,
+		productId: 3,
+		SKU:"YSL-BOP-100",
+		quantity: 26,
+	},
+];
+
+exports.wishProducts = [
+	{
+		wishListId: 1,
+		productId: 1,
+		SKU:"DIO-SAU-30",
+	},
+	{
+		wishListId: 1,
+		productId: 2,
+		SKU:"CHA-BLE-60",
+	},
+	{
+		wishListId: 1,
+		productId: 3,
+		SKU:"YSL-BOP-100",
+	},
+];
+
